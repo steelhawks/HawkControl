d8af33f34e1b22eda21e59681e1bdeb9c8cf58fe
Farhan Jamil <71662961+FarhanJ2@users.noreply.github.com>
2024-06-28 14:35:17 -0400

    Initial Commit

---
+++ HawkControl/HawkControlApp.swift
@@ -0,0 +1,17 @@
+//
+//  HawkControlApp.swift
+//  HawkControl
+//
+//  Created by Farhan Jamil on 6/28/24.
+//
+
+import SwiftUI
+
+@main
+struct HawkControlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ HawkControlTests/HawkControlTests.swift
@@ -0,0 +1,17 @@
+//
+//  HawkControlTests.swift
+//  HawkControlTests
+//
+//  Created by Farhan Jamil on 6/28/24.
+//
+
+import Testing
+@testable import HawkControl
+
+struct HawkControlTests {
+
+    @Test func testExample() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
